7cfb96e3d5d47e3c5bce12c5a06d8804f37c2112
Jessica Melo <jessicamariademelo@hotmail.com>
2019-08-18 21:34:27 -0300

    no message

--- VetoresMatrizes/src/Exercicio17.java
+++ VetoresMatrizes/src/Exercicio17.java
@@ -6,26 +6,31 @@ public class Exercicio17 {
 	public static void main(String[] args) {
 
 		int Seq1[] = { 8, 2, 4, 3, 4, 2, 5, 1 };
-		int Seq2[] = { 2, 1, 9, 5, 2, 3, 6, 7 };
+		int Seq2[] = { 0, 1, 9, 5, 2, 3, 6, 7 };
 		int Result[] = new int[Seq1.length + 1];
 		int valor = 0;// guardar sobra
 		int estouro = 0;
 
 		// for (int j = 7; j > Seq1.length - 1; j--) {
-		for (int i = Seq1.length-1; i >= 0; i--) {
+		for (int i = Seq1.length - 1; i >= 0; i--) {
 			valor = Seq1[i] + Seq2[i] + estouro;
 			estouro = 0;
 			if (valor > 9) {
-				 estouro = 1;
+				estouro = 1;
 			}
-			Result[i+1] = valor % 10;
+			Result[i + 1] = valor % 10;
 
 		}
 		Result[0] = estouro;
+
 		// }
 		for (int i = 0; i < Result.length; i++) {
-			System.out.println(Result[i]);
-
+			if (i == 0 && Result[i] != 0) {
+				System.out.println(Result[i]);
+			}
+			if (i != 0) {
+				System.out.println(Result[i]);
+			}
 		}
 
 	}
